a2e4def23a57cf7f5f92ac01acdc9071d060769a
jramscr <jramscr@gmail.com>
2014-10-23 09:54:55 -0600

    Iteration 2. Arreglo final de inicializacion de postgres a nivel loca.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'home/show'
 
+  get 'layouts/auction'
+
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
