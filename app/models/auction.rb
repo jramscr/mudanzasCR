cc017256a2a29a051253bcfd242d337fa029a292
jramscr <jramscr@gmail.com>
2014-11-03 16:42:11 -0600

    Iteration 2. First changes on the css files for profile and home page

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -2,7 +2,9 @@ class Auction < ActiveRecord::Base
     include ActiveModel::ForbiddenAttributesProtection
     belongs_to :user
 
-    validates :direction_to_pick , presence: true
-    validates :direction_to_deliver, presence: true
+    validates :start_auction_at,:end_auction_at, :hour_to_pick, :direction_to_pick, :hour_to_deliver, :direction_to_deliver, :budget , presence: true
     validates :budget , numericality: {only_integer: true}
+    validates :start_auction_at, numericality: {greater_than_or_equal_to: 1, less_than: 24}
+    validates :end_auction_at, numericality: {greater_than: :start_auction_at}
+    validates :hour_to_deliver, numericality: {greater_than: :hour_to_pick}
 end
