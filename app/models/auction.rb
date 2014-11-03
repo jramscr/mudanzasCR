4c0c8961aad122ebe6ec6da2e2aa7075a96cb9d1
jramscr <jramscr@gmail.com>
2014-11-03 12:04:39 -0600

    Iteration 2. Some validations in the auction model.

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,3 +1,8 @@
 class Auction < ActiveRecord::Base
     include ActiveModel::ForbiddenAttributesProtection
+    belongs_to :user
+
+    validates :direction_to_pick , presence: true
+    validates :direction_to_deliver, presence: true
+    validates :budget , numericality: {only_integer: true}
 end
